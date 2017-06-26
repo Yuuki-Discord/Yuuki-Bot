fc7d6b2277193895cfea768fd293f570a77ffb30
Erisa A <seriel@erisa.moe>
2017-06-26 21:10:42 +0100

    Batch file skeletons

--- first_run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-gem install bundler
-bundle install
-bundle exec ruby run.rb
-pause
---
+++ modules/memes/spinner.rb
@@ -0,0 +1,14 @@
+# Copyright Seriel 2017
+module YuukiBot
+  module Memes
+
+    $cbot.add_command(:spinner,
+      code: proc { |event,args|
+        if args[0] == 'red'
+
+        end
+      }
+    )
+
+  end
+end
