5d30f9710aa154284dd8cc3238ae62961a3c49f8
Seriel <seriel@fl0.co>
2017-07-07 04:15:19 +0100

    Embeds are the future

--- install_or_update.bat
+++ install_or_update.bat
@@ -1,8 +1,8 @@
 @echo off
 echo :: Installing/Updating Bundler
-gem install bundler
+call gem install bundler
 echo :: Installing/Udating Bundle..
-bundle install
+call bundle install
 echo :: Updating Yuuki-Bot..
 git pull
 echo Done! Please load run.bat if successful!
