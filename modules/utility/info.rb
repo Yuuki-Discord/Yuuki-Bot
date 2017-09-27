6e5eec5b9c25b88b9852f14fa997d5a9d46da646
Erisa A <seriel@erisa.moe>
2017-09-27 22:46:22 +0100

    Error Embed and Avatar Embed helper functions created.

--- modules/utility/info.rb
+++ modules/utility/info.rb
@@ -5,19 +5,31 @@ module YuukiBot
 
     $cbot.add_command(:avatar,
       code: proc { |event, args|
-        begin
-          user = event.bot.parse_mention(args[0])
-        rescue
+        if args.length == 0
           user = event.user
+        else
+          begin
+            user = event.bot.parse_mention(args.join(' '))
+            puts user
+          rescue
+            event.channel.send_message('', false,
+              Helper.error_embed(
+               error: "Not a valid user!",
+               footer: "Command: `#{event.message.content}`",
+               colour: 0x22ef1f,
+               code_error: false
+              )
+            )
+            raise 'Not a valid user'
+          end
         end
-        # event.channel.send_file File.new(Helper.download_avatar(user, 'tmp'))
-		event.channel.send_embed do |embed|
-		  embed.colour = 0x22ef1f
 
-		  embed.image = Discordrb::Webhooks::EmbedImage.new(url: Helper.avatar_url(user))
-		  embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "Avatar for #{user.name}", url: Helper.avatar_url(user))
-		  embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "Avatar correct as of #{Time.now.getutc.asctime}")
-		end
+        event.channel.send_embed do |embed|
+          embed.colour = 0x22ef1f
+          embed.image = Discordrb::Webhooks::EmbedImage.new(url: Helper.avatar_url(user))
+          embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "Avatar for #{user.name}", url: Helper.avatar_url(user))
+          embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "Avatar correct as of #{Time.now.getutc.asctime}")
+        end
       },
       triggers: %w(avatar avy),
       :server_only => true,
@@ -32,14 +44,6 @@ module YuukiBot
         end
 
         member = user.on(event.server) unless event.channel.private?
-        # event <<  "#{Data.donators.include?(user.id) ? ' 👑' : ' 👥' } Infomation about **#{(event.channel.private? ? user.name : member.display_name)}**"
-        # event << "-ID: **#{user.id}**"
-        # event << "-Username: `#{user.distinct}`"
-        # event << "-Nickname: **#{member.nickname.nil? ? '[N/A]' : member.nickname}**" unless event.channel.private?
-        # event << "-Status: **#{user.status}**"
-        # event << "-Playing: **#{user.game.nil? ? '[N/A]' : user.game}**"
-        # event << "-Account created: **#{user.creation_time.getutc.asctime}** UTC"
-        # event << "-Joined server at: **#{member.joined_at.getutc.asctime}** UTC" unless event.channel.private?
         nickname = member.nickname.nil? ? member.display_name : member.nickname unless event.channel.private?
         event.channel.send_embed("__Information about **#{user.distinct}**__") do |embed|
           embed.colour = event.channel.private? ? 0xe06b2 : Helper.colour_from_user(member)
