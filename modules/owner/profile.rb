71e78140082763211a7f88f06cd3d69aa0cb4cd3
Erisa A <seriel@erisa.moe>
2017-12-23 21:42:49 +0000

    Add nickname change support & reset command.

--- modules/owner/profile.rb
+++ modules/owner/profile.rb
@@ -5,11 +5,18 @@ module YuukiBot
     $cbot.add_command(:set,
       code: proc {|event,args|
         case args[0].downcase
+        when 'nick' || 'nickname'
+          begin
+            event.bot.profile.on(event.server).nickname = args.drop(1).join(' ')
+            event.respond("#{YuukiBot.config['emoji_tickbox']} Nickname changed to `#{args.drop(1).join(' ')}`")
+          rescue Discordrb::Errors::NoPermission
+            event.respond("#{YuukiBot.config['emoji_error']} I don't have permission to change my nickname!")
+          end
         when 'avatar' || 'avy'
           url = args.drop(1).join(' ')
           event.bot.profile.avatar = File.open(Helper.download_file(url, 'tmp'))
         when 'game' || 'playing'
-          event.bot.game = args.join(' ')
+          event.bot.game = args.drop(1).join(' ')
           event.respond("#{YuukiBot.config['emoji_tickbox']} Game set to `#{args.drop(1).join(' ')}`!")
         when 'username' || 'name'
             username = args.drop(1).join(' ')
@@ -33,13 +40,38 @@ module YuukiBot
           end
           event.respond("#{YuukiBot.config['emoji_tickbox']} Status set to **#{state.capitalize}**!")
         else
-          event.respond("#{YuukiBot.config['emoji_error']} Enter a valid subcommand!'\nValid commands: `avatar`, `game`, `username`, `status`.")
+          event.respond("#{YuukiBot.config['emoji_error']} Enter a valid subcommand!\nValid commands: `avatar`, `game`, `username`, `status`, `nickname`.")
           next
         end
       },
       triggers: %w(set config bot),
       owners_only: true
     )
+    $cbot.add_command(:reset,
+      code: proc {|event,args|
+        case args[0].downcase
+          when 'nick' || 'nickname'
+            begin
+              event.bot.profile.on(event.server).nickname = nil
+              event.respond("#{YuukiBot.config['emoji_tickbox']} Reset nickname!")
+            rescue Discordrb::Errors::NoPermission
+              event.respond("#{YuukiBot.config['emoji_error']} I don't have permission to change my nickname!")
+            end
+          when 'avatar' || 'avy'
+            event.bot.avatar = nil
+            event.respond "#{YuukiBot.config['emoji_success']} Reset avatar!"
+          when 'game' || 'playing'
+            event.bot.game = nil
+            event.respond("#{YuukiBot.config['emoji_tickbox']} Reset game!")
+          else
+            event.respond("#{YuukiBot.config['emoji_error']} Enter a valid subcommand!\nValid commands: `avatar`, `game`, `nickname`.")
+            next
+        end
+      },
+      triggers: %w(reset),
+      owners_only: true
+    )
+
 
   end
 end
