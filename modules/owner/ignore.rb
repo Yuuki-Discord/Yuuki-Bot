003af4f11d22d1971f3473606d301afe92707733
Erisa A <seriel@erisa.moe>
2017-12-27 20:10:13 +0000

    ignore hotfix

--- modules/owner/ignore.rb
+++ modules/owner/ignore.rb
@@ -5,27 +5,25 @@ module YuukiBot
     $cbot.add_command(:ignore,
       code: proc { |event, args|
         event.respond("#{YuukiBot.config['emoji_error']} Mention valid user(s)!") if args == []
-        args.each { |x|
-          mention = args[0]
-          user = begin
-            event.bot.parse_mention(x)
-          rescue
-            event.respond("#{YuukiBot.config['emoji_error']} `#{mention}` is not a valid user!")
-            break
+        mention = args[0]
+        user = begin
+          event.bot.parse_mention(x)
+        rescue
+          event.respond("#{YuukiBot.config['emoji_error']} `#{mention}` is not a valid user!")
+          break
+        end
+        begin
+          userdata = DB.execute("SELECT * FROM `userlist` WHERE `id` = #{user.id}")
+          if userdata == []
+            DB.execute("INSERT INTO userlist (id, is_owner, is_donator, ignored) VALUES (#{user.id}, 0, 0, 1); ")
+          else
+            DB.execute("UPDATE userlist SET ignored = 1 WHERE id = #{user.id};")
           end
-          begin
-            userdata = DB.execute("SELECT * FROM `userlist` WHERE `id` = #{user.id}")
-            if userdata == []
-              DB.execute("INSERT INTO userlist (id, is_owner, is_donator, ignored) VALUES (#{user.id}, 0, 0, 1); ")
-            else
-              DB.execute("UPDATE userlist SET ignored = 1 WHERE id = #{user.id};")
-            end
-            event.bot.ignore_user(user)
-            event.respond("#{YuukiBot.config['emoji_tickbox']} #{user.mention} is now being ignored!")
-          rescue
-            event.respond("#{YuukiBot.config['emoji_error']} Error occured, is `#{mention}` a valid use?")
-          end
-        }
+          event.bot.ignore_user(user)
+          event.respond("#{YuukiBot.config['emoji_tickbox']} #{user.mention} is now being ignored!")
+        rescue
+          event.respond("#{YuukiBot.config['emoji_error']} Error occured, is `#{mention}` a valid user?")
+        end
       },
       triggers: %w(ignore),
       owners_only: true
