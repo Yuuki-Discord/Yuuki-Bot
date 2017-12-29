fbe008b64e4c7f4383ed5fa24b93eba90b501a7d
U-Erisa-PC\Erisa <seriel@fl0.co>
2017-12-29 20:26:02 +0000

    Nil check for :set, fixes #18

--- modules/owner/profile.rb
+++ modules/owner/profile.rb
@@ -4,44 +4,48 @@ module YuukiBot
     
     $cbot.add_command(:set,
       code: proc {|event,args|
-        case args[0].downcase
-        when 'nick' || 'nickname'
-          begin
-            event.bot.profile.on(event.server).nickname = args.drop(1).join(' ')
-            event.respond("#{YuukiBot.config['emoji_tickbox']} Nickname changed to `#{args.drop(1).join(' ')}`")
-          rescue Discordrb::Errors::NoPermission
-            event.respond("#{YuukiBot.config['emoji_error']} I don't have permission to change my nickname!")
-          end
-        when 'avatar' || 'avy'
-          url = args.drop(1).join(' ')
-          event.bot.profile.avatar = File.open(Helper.download_file(url, 'tmp'))
-        when 'game' || 'playing'
-          event.bot.game = args.drop(1).join(' ')
-          event.respond("#{YuukiBot.config['emoji_tickbox']} Game set to `#{args.drop(1).join(' ')}`!")
-        when 'username' || 'name'
-            username = args.drop(1).join(' ')
-          event.bot.profile.name = begin
-            username
-          rescue
-            event.respond('An error has occured!')
-          end
-          event.respond("#{YuukiBot.config['emoji_tickbox']} Username _should_ be updated!")
-        when 'status' || 'indicator'
-          state = args[1]
-          case state.downcase
-          when 'idle' || 'away' || 'afk' then event.bot.idle
-          when 'dnd'
-            state = 'Do Not Disturb'
-          when 'online' then event.bot.online
-          when 'invisible' || 'offline' then event.bot.invisible
+        if args.length.zero?
+          event.respond("#{YuukiBot.config['emoji_error']} Enter a valid subcommand!\nValid commands: `avatar`, `game`, `username`, `status`, `nickname`.")
+        else
+          case args[0].downcase
+          when 'nick' || 'nickname'
+            begin
+              event.bot.profile.on(event.server).nickname = args.drop(1).join(' ')
+              event.respond("#{YuukiBot.config['emoji_tickbox']} Nickname changed to `#{args.drop(1).join(' ')}`")
+            rescue Discordrb::Errors::NoPermission
+              event.respond("#{YuukiBot.config['emoji_error']} I don't have permission to change my nickname!")
+            end
+          when 'avatar' || 'avy'
+            url = args.drop(1).join(' ')
+            event.bot.profile.avatar = File.open(Helper.download_file(url, 'tmp'))
+          when 'game' || 'playing'
+            event.bot.game = args.drop(1).join(' ')
+            event.respond("#{YuukiBot.config['emoji_tickbox']} Game set to `#{args.drop(1).join(' ')}`!")
+          when 'username' || 'name'
+              username = args.drop(1).join(' ')
+            event.bot.profile.name = begin
+              username
+            rescue
+              event.respond('An error has occured!')
+            end
+            event.respond("#{YuukiBot.config['emoji_tickbox']} Username _should_ be updated!")
+          when 'status' || 'indicator'
+            state = args[1]
+            case state.downcase
+            when 'idle' || 'away' || 'afk' then event.bot.idle
+            when 'dnd'
+              state = 'Do Not Disturb'
+            when 'online' then event.bot.online
+            when 'invisible' || 'offline' then event.bot.invisible
+            else
+              event.respond('Enter a valid argument!')
+              next
+            end
+            event.respond("#{YuukiBot.config['emoji_tickbox']} Status set to **#{state.capitalize}**!")
           else
-            event.respond('Enter a valid argument!')
+            event.respond("#{YuukiBot.config['emoji_error']} Enter a valid subcommand!\nValid commands: `avatar`, `game`, `username`, `status`, `nickname`.")
             next
           end
-          event.respond("#{YuukiBot.config['emoji_tickbox']} Status set to **#{state.capitalize}**!")
-        else
-          event.respond("#{YuukiBot.config['emoji_error']} Enter a valid subcommand!\nValid commands: `avatar`, `game`, `username`, `status`, `nickname`.")
-          next
         end
       },
       triggers: %w(set config),
