1b100cea6c4e08722c4d0766bf5971d807208b8b
Erisa A <seriel@erisa.moe>
2018-01-26 20:21:42 +0000

    Finally get around to implementing custom configs!
    Check config/custom/README.md for details.

---
+++ modules/custom.rb
@@ -0,0 +1,54 @@
+module YuukiBot
+  module Custom
+
+    if File.exists?('config/custom/custom.rb')
+      require './config/custom/custom.rb'
+      unless CUSTOM_TEXT.nil? || CUSTOM_TEXT == {}
+        CUSTOM_TEXT.each {|name, response|
+          $cbot.add_command(name.to_sym,
+           code: proc {|event|
+             event.respond(response)
+           }
+          )
+          puts "Added custom command: #{name}"
+        }
+      end
+
+      unless CUSTOM_IMAGE.nil? || CUSTOM_IMAGE == {}
+        CUSTOM_IMAGE.each {|name, response|
+          $cbot.add_command(name.to_sym,
+           code: proc {|event|
+             event.respond(response)
+           }
+          )
+          puts "Added custom command: #{name}"
+        }
+      end
+    end
+
+    yml_commands = YAML.load_file('./config/custom/custom.yml') if File.exists?('config/custom/custom.yml')
+    unless yml_commands.nil? || yml_commands[:text].nil? || yml_commands[:text] == {}
+      yml_commands[:text].each {|name, response|
+        $cbot.add_command(name.to_sym,
+         code: proc {|event|
+           event.respond(response)
+         }
+        )
+        puts "Added custom command: #{name}"
+      }
+    end
+
+    unless yml_commands.nil? || yml_commands[:image].nil? || yml_commands[:image] == {}
+      yml_commands[:image].each {|name, response|
+        $cbot.add_command(name.to_sym,
+         code: proc {|event|
+           event.respond(response)
+         }
+        )
+        puts "Added custom command: #{name}"
+      }
+    end
+    require './config/custom/code.rb' if File.exists? 'config/custom/code.rb'
+
+  end
+end
