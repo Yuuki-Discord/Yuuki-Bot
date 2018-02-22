5c392447e4e7256798cb4d3375b719c40f87df71
Erisa A <seriel@erisa.moe>
2018-02-22 23:52:03 +0000

    works now o/

--- modules/owner/eval.rb
+++ modules/owner/eval.rb
@@ -32,7 +32,7 @@ module YuukiBot
           result = eval args.join(' ')
           result = result.to_s
           if result.nil? || result == '' || result == ' ' || result == "\n"
-            event << "#{YuukiBot.config['emoji_tickbox']} Done! (No output)\nCommand took #{(Time.now - init_time)} seconds to execute!"
+            msg.edit "#{YuukiBot.config['emoji_tickbox']} Done! (No output)\nCommand took #{(Time.now - init_time)} seconds to execute!"
             next
           end
           str = ''
@@ -55,14 +55,22 @@ module YuukiBot
     $cbot.add_command(:bash,
       code: proc { |event, args|
         init_time = Time.now
+        msg = event.respond "#{YuukiBot.config['emoji_loading']} Evaluating..."
         # Capture all output, including STDERR.
         result = `#{"#{args.join(' ')} 2>&1"} `
-        if result.length >= 1984
-          event << "#{YuukiBot.config['emoji_warning']} Your output exceeded the character limit by `#{result.length - 1984}` characters!"
-          event << "You can view the result here: https://hastebin.com/raw/#{$uploader.upload_raw(result)}\nCommand took #{(Time.now - init_time)} seconds to execute!"
-        else
-          event << ((result.nil? || result == '' || result == ' ' || result == "\n") ? "#{YuukiBot.config['emoji_tickbox']} Done! (No output)\nCommand took #{(Time.now - init_time)} seconds to execute!" : "Output: ```\n#{result}```Command took #{(Time.now - init_time)} seconds to execute!")
+        result = result.to_s
+        if result.nil? || result == '' || result == ' ' || result == "\n"
+          msg.edit "#{YuukiBot.config['emoji_tickbox']} Done! (No output)\nCommand took #{(Time.now - init_time)} seconds to execute!"
+          next
         end
+        str = ''
+        if result.length >= 1984
+          str << "#{YuukiBot.config['emoji_warning']} Your output exceeded the character limit! (`#{result.length - 1984}`/`1984`)"
+          str << "You can view the result here: https://hastebin.com/raw/#{$uploader.upload_raw(result)}\nCommand took #{(Time.now - init_time)} seconds to execute!"
+        else
+          str << "Output: ```\n#{result}```Command took #{(Time.now - init_time)} seconds to execute!"
+        end
+        msg.edit(str)
       },
       triggers: ['bash ', 'sh ', 'shell ', 'run '],
       owners_only: true,
