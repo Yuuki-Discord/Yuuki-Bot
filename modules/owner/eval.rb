f6d7219e9dfd14cda5f93dd37d9fee2efe4ae8da
Erisa A <seriel@erisa.moe>
2018-02-22 23:45:20 +0000

    because we all wanted to know how long evals take

--- modules/owner/eval.rb
+++ modules/owner/eval.rb
@@ -27,21 +27,24 @@ module YuukiBot
     $cbot.add_command(:eval,
         code: proc { |event, args|
         begin
+          msg = event.respond "#{YuukiBot.config['emoji_loading']} Evaluating..."
+          init_time = Time.now
           result = eval args.join(' ')
           result = result.to_s
           if result.nil? || result == '' || result == ' ' || result == "\n"
-            event << "#{YuukiBot.config['emoji_tickbox']} Done! (No output)"
+            event << "#{YuukiBot.config['emoji_tickbox']} Done! (No output)\nCommand took #{(Time.now - init_time)} seconds to execute!"
             next
           end
-
+          str = ''
           if result.length >= 1984
-            event << "#{YuukiBot.config['emoji_warning']} Your output exceeded the character limit! (`#{result.length - 1984}`/`1984`)"
-            event << "You can view the result here: https://hastebin.com/raw/#{$uploader.upload_raw(result)}"
+            str << "#{YuukiBot.config['emoji_warning']} Your output exceeded the character limit! (`#{result.length - 1984}`/`1984`)"
+            str << "You can view the result here: https://hastebin.com/raw/#{$uploader.upload_raw(result)}\nCommand took #{(Time.now - init_time)} seconds to execute!"
           else
-            event << "Output: ```\n#{result}```"
+            str << "Output: ```\n#{result}```Command took #{(Time.now - init_time)} seconds to execute!"
           end
+          msg.edit(str)
           rescue Exception => e
-          event.respond("#{YuukiBot.config['emoji_error']} An error has occured!! ```ruby\n#{e}```")
+          msg.edit("#{YuukiBot.config['emoji_error']} An error has occured!! ```ruby\n#{e}```\nCommand took #{(Time.now - init_time)} seconds to execute!")
         end
         },
         triggers: ['eval2 ', 'eval'],
@@ -51,13 +54,14 @@ module YuukiBot
 
     $cbot.add_command(:bash,
       code: proc { |event, args|
+        init_time = Time.now
         # Capture all output, including STDERR.
         result = `#{"#{args.join(' ')} 2>&1"} `
         if result.length >= 1984
           event << "#{YuukiBot.config['emoji_warning']} Your output exceeded the character limit by `#{result.length - 1984}` characters!"
-          event << "You can view the result here: https://hastebin.com/raw/#{$uploader.upload_raw(result)}"
+          event << "You can view the result here: https://hastebin.com/raw/#{$uploader.upload_raw(result)}\nCommand took #{(Time.now - init_time)} seconds to execute!"
         else
-          event << ((result.nil? || result == '' || result == ' ' || result == "\n") ? "#{YuukiBot.config['emoji_tickbox']} Done! (No output)" : "Output: ```\n#{result}```")
+          event << ((result.nil? || result == '' || result == ' ' || result == "\n") ? "#{YuukiBot.config['emoji_tickbox']} Done! (No output)\nCommand took #{(Time.now - init_time)} seconds to execute!" : "Output: ```\n#{result}```Command took #{(Time.now - init_time)} seconds to execute!")
         end
       },
       triggers: ['bash ', 'sh ', 'shell ', 'run '],
