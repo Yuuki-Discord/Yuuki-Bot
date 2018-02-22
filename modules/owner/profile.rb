1d6bdb3c702ca7e1a382fac705357bcf082aacb6
Erisa A <seriel@erisa.moe>
2018-02-22 23:44:59 +0000

    lol this actually works now, took it long enough

--- modules/owner/profile.rb
+++ modules/owner/profile.rb
@@ -8,40 +8,40 @@ module YuukiBot
           event.respond("#{YuukiBot.config['emoji_error']} Enter a valid subcommand!\nValid commands: `avatar`, `game`, `username`, `status`, `nickname`.")
         else
           case args[0].downcase
-          when 'nick' || 'nickname'
+          when 'nick', 'nickname'
             begin
               event.bot.profile.on(event.server).nickname = args.drop(1).join(' ')
               event.respond("#{YuukiBot.config['emoji_tickbox']} Nickname changed to `#{args.drop(1).join(' ')}`")
             rescue Discordrb::Errors::NoPermission
               event.respond("#{YuukiBot.config['emoji_error']} I don't have permission to change my nickname!")
             end
-          when 'avatar' || 'avy'
+          when 'avatar', 'avy'
             url = args.drop(1).join(' ')
             event.bot.profile.avatar = File.open(Helper.download_file(url, 'tmp'))
-          when 'game' || 'playing'
+          when 'game', 'playing'
             event.bot.game = args.drop(1).join(' ')
             event.respond("#{YuukiBot.config['emoji_tickbox']} Game set to `#{args.drop(1).join(' ')}`!")
-          when 'username' || 'name'
+          when 'username', 'name'
               username = args.drop(1).join(' ')
             event.bot.profile.name = begin
               username
-            rescue
-              event.respond('An error has occured!')
+            rescue Error => e
+              event.respond("#{YuukiBot.config['emoji_error']} An error has occured!\n```ruby\n#{e}```")
             end
             event.respond("#{YuukiBot.config['emoji_tickbox']} Username _should_ be updated!")
-          when 'status' || 'indicator'
+          when 'status', 'indicator', 'state'
             state = args[1]
             case state.downcase
-            when 'idle' || 'away' || 'afk' then event.bot.idle
+            when 'idle', 'away', 'afk' then event.bot.idle
             when 'dnd'
               state = 'Do Not Disturb'
             when 'online' then event.bot.online
-            when 'invisible' || 'offline' then event.bot.invisible
+            when 'invisible', 'offline' then event.bot.invisible
             else
               event.respond('Enter a valid argument!')
               next
             end
-            event.respond("#{YuukiBot.config['emoji_tickbox']} Status set to **#{state.capitalize}**!")
+            event.respond("#{YuukiBot.config['emoji_tickbox']} Status set to **#{state == 'Do Not Disturb' ? state : state.capitalize }**!")
           else
             event.respond("#{YuukiBot.config['emoji_error']} Enter a valid subcommand!\nValid commands: `avatar`, `game`, `username`, `status`, `nickname`.")
             next
