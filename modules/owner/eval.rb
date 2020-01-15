711a284c83a66d57d87e99337854d7205a30fe84
Erisa <seriel@erisa.moe>
2020-01-15 23:12:11 +0000

    Fix handling of 'owners' cmd when no master owner

--- modules/owner/eval.rb
+++ modules/owner/eval.rb
@@ -16,7 +16,7 @@ module YuukiBot
       code: proc { |event, _|
         owners = JSON.parse(REDIS.get('owners')) rescue []
         event << 'This bot instance is managed/owned by the following users. Please contact them for any issues.'
-        event << "- **#{event.bot.user(YuukiBot.config['master_owner']).distinct}** [**MAIN**]"
+        event << "- **#{event.bot.user(YuukiBot.config['master_owner']).distinct}** [**MAIN**]" unless YuukiBot.config['master_owner'].nil?
         owners.each {|x|
           event.bot.user(x).nil? ? event << "- Unknown User (ID: `#{x}`)" : event << "- **#{event.bot.user(x).distinct}**"
         }
