598ef85fddd5981c98f38ee2406d74b47c0ec329
Erisa A <seriel@erisa.moe>
2020-01-15 22:57:36 +0000

    Remove the 'about' command

--- modules/misc/helpabout.rb
+++ modules/misc/helpabout.rb
@@ -1,28 +1,7 @@
 # Copyright Erisa Arrowsmith 2016 - 2017
 module YuukiBot
   module Misc
-
-    $cbot.add_command(:about,
-      code: proc { |event, _|
-        event << "`#{event.bot.user(event.bot.profile.id).distinct}` running **YuukiBot v#{$version}** "
-        event << "**#{YuukiBot.config['source_url']}** " if YuukiBot.config['show_source']
-        event << "\n ⚙ Extra commands: **#{YuukiBot.config['extra_commands'] ? 'Enabled' : 'Disabled'}**"
-        if YuukiBot.config['show_donate_urls']
-          event << "\n:moneybag: Hey, making bots and hosting them isn't free. If you want this bot to stay alive, consider giving some :dollar: to the devs: "
-          YuukiBot.config['donate_urls'].each {|url| event << "- #{url}" }
-          event << "\n__**Donators :heart:**__ (aka the best people ever)"
-          donators = DB.execute("select id from userlist where is_donator=1").map {|v| v[0]}
-          if donators.length > 0
-            donators.each {|x|
-              event.bot.user(x).nil? ? event << "Unknown User (ID: `#{x}`)" : event << "`- **#{event.bot.user(x).distinct}**"
-            }
-          else
-            event << 'None! You can be the first! :)'
-          end
-        end
-      }
-    )
-
+    
     $cbot.add_command(:help,
       code: proc {|event, _|
         event << (YuukiBot.config['show_help'] ? "Follow this link for basic help: ** 🔗 #{YuukiBot.config['help_url']}**" : 'Unfortunately, no command help can be shown. Please contact the bot owner.')
