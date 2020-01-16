126dd024552fa18d028a885dd93d1afc383a8686
Erisa <seriel@erisa.moe>
2020-01-16 00:06:56 +0000

    Move botowners to a more appropriate file

--- modules/owner/owners.rb
+++ modules/owner/owners.rb
@@ -13,6 +13,38 @@ module YuukiBot
             },
             triggers: ['owners']
         )
-        
+
+        $cbot.add_command(:botowners,
+            code: proc { |event, args|
+              user = Helper.userparse(args[1])
+              id = user.id rescue nil
+              if user.nil?
+                event.respond("#{YuukiBot.config['emoji_error']} Not a valid user!")
+              else
+                if args[0] == 'add'
+                  owners = JSON.parse(REDIS.get('owners'))
+                  if $cbot.is_owner?(id)
+                    event.respond("#{YuukiBot.config['emoji_error']} User is already an owner!")
+                    next
+                  end
+                  REDIS.set('owners', owners.push(id).to_json)
+                  event.respond("#{YuukiBot.config['emoji_tickbox']} added `#{event.bot.user(id).nil? ? "Unknown User (ID: #{id})" : "#{event.bot.user(id).distinct}"}` to bot owners!")
+                elsif args[0] == 'remove'
+                  owners = JSON.parse(REDIS.get('owners'))
+                  unless owners.include?(id)
+                    event.respond("#{YuukiBot.config['emoji_error']} User is not an owner!")
+                    next
+                  end
+                  if YuukiBot.config['master_owner'] == id
+                    event.respond("#{YuukiBot.config['emoji_error']} You can't remove the main owner!")
+                  end
+                  REDIS.set('owners', owners.delete(id).to_json)
+                  event.respond("#{YuukiBot.config['emoji_tickbox']} removed `#{event.bot.user(id).nil? ? "Unknown User (ID: #{id})" : "#{event.bot.user(id).distinct}"}` from bot owners!")
+                end
+              end
+            },
+            owners_only: true
+          )
+
     end
 end
