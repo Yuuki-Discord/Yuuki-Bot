a4bebaa58d277b74304d20fc5d93840b66a14277
Spotlight <spotlight@joscomputing.space>
2020-01-19 18:56:51 -0600

    Concatenate eval handling logic
    This allows shared logic between bash and the new eval command, specifically.

--- modules/owner/eval.rb
+++ modules/owner/eval.rb
@@ -8,7 +8,7 @@ module YuukiBot
                       code: proc { |event, args|
                         event.respond(eval(args.join(' ')))
                       },
-                      triggers: ['raweval ', 'oldeval'],
+                      triggers: ['raweval ', 'oldeval '],
                       owners_only: true)
 
     $cbot.add_command(:eval,
@@ -17,21 +17,12 @@ module YuukiBot
                           msg = event.respond "#{YuukiBot.config['emoji_loading']} Evaluating..."
                           init_time = Time.now
                           result = eval args.join(' ')
-                          result = result.to_s
-                          if result.nil? || result == '' || result == ' ' || result == "\n"
-                            msg.edit "#{YuukiBot.config['emoji_tickbox']} Done! (No output)\nCommand took #{(Time.now - init_time)} seconds to execute!"
-                            next
-                          end
-                          str = ''
-                          if result.length >= 1984
-                            str << "#{YuukiBot.config['emoji_warning']} Your output exceeded the character limit! (`#{result.length - 1984}`/`1984`)"
-                            str << "You can view the result here: https://paste.erisa.moe/raw/#{$uploader.upload_raw(result)}\nCommand took #{(Time.now - init_time)} seconds to execute!"
-                          else
-                            str << "Output: ```\n#{result}```Command took #{(Time.now - init_time)} seconds to execute!"
-                          end
-                          msg.edit(str)
+                          result_output = handle_result(result, init_time)
+                          msg.edit(result_output)
                         rescue Exception => e
-                          msg.edit("#{YuukiBot.config['emoji_error']} An error has occured!! ```ruby\n#{e}```\nCommand took #{(Time.now - init_time)} seconds to execute!")
+                          msg.edit("#{YuukiBot.config['emoji_error']} An error has occurred!"\
+                          "```ruby\n#{e}```" \
+                          "Command took #{(Time.now - init_time)} seconds to execute!")
                         end
                       },
                       triggers: ['eval2 ', 'eval'],
@@ -44,22 +35,38 @@ module YuukiBot
                         msg = event.respond "#{YuukiBot.config['emoji_loading']} Evaluating..."
                         # Capture all output, including STDERR.
                         result = `#{"#{args.join(' ')} 2>&1"} `
-                        result = result.to_s
-                        if result.nil? || result == '' || result == ' ' || result == "\n"
-                          msg.edit "#{YuukiBot.config['emoji_tickbox']} Done! (No output)\nCommand took #{(Time.now - init_time)} seconds to execute!"
-                          next
-                        end
-                        str = ''
-                        if result.length >= 1984
-                          str << "#{YuukiBot.config['emoji_warning']} Your output exceeded the character limit! (`#{result.length - 1984}`/`1984`)"
-                          str << "You can view the result here: https://paste.erisa.moe/raw/#{$uploader.upload_raw(result)}\nCommand took #{(Time.now - init_time)} seconds to execute!"
-                        else
-                          str << "Output: ```\n#{result}```Command took #{(Time.now - init_time)} seconds to execute!"
-                        end
-                        msg.edit(str)
+                        result_output = handle_result(result, init_time)
+                        msg.edit(result_output)
                       },
                       triggers: ['bash ', 'sh ', 'shell ', 'run '],
                       owners_only: true,
                       description: 'Evaluate a Bash command. Owner only. Use with care.')
+
+    # Formulates results to an external source or character-specific message.
+    # @param [Object] result_output Returned result of an operation.
+    # @param [Class<Time>] start_time The starting time of execution.
+    # @param [Class<Time>] finish_time The ending time of execution.
+    # @return [String (frozen)] Human-readable format of given execution
+    def self.handle_result(result_output, start_time, finish_time = Time.now)
+      output = ''
+
+      result = result_output.to_s
+      if result.nil? || result == '' || result == ' ' || result == "\n"
+        output += '' "#{YuukiBot.config['emoji_tickbox']} Done! (No output)"
+      elsif result.length >= 1984
+        uploader_domain = $uploader.server_url
+        uploader_file = $uploader.upload_raw(result)
+
+        output += "#{YuukiBot.config['emoji_warning']}" \
+        'Your output exceeded the character limit! ' \
+        "(`#{result.length - 1984}`/`1984`)\n" \
+        "You can view the result here: #{uploader_domain}/raw/#{uploader_file}"
+      else
+        output += "Output: ```\n#{result}```"
+      end
+
+      output += "Command took #{(finish_time - start_time)} seconds to execute!"
+      output
+    end
   end
 end
