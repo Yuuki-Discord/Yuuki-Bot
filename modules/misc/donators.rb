2bc9d7539bff903191362b8536c3f1a6ffa3af56
Spotlight <spotlight@joscomputing.space>
2020-02-24 17:16:24 -0600

    Make `donators` share common code
    This also fixes a pesky issue involving the direct result of Array#delete -> obj overwriting the value with obj itself. ..oops

--- modules/misc/donators.rb
+++ modules/misc/donators.rb
@@ -6,117 +6,79 @@ module YuukiBot
     YuukiBot.crb.add_command(
       :donators,
       code: proc { |event, args|
+        if args.empty?
+          # Treat as normal donate.
+          common_donate(event)
+          next
+        end
+        valid_commands = %w[add remove]
+        unless valid_commands.include?(args[0]) || args[1].nil?
+          event.respond("#{YuukiBot.config['emoji_error']} Not a valid command! Use me with `add @user` or `remove @user`.")
+          next
+        end
+
         user = Helper.userparse(args[1])
         id = begin
-  user.id
+          user.id
              rescue StandardError
-               nil
-end
+               event.respond("#{YuukiBot.config['emoji_error']} Not a valid user!")
+               next
+        end
+
+        donators = begin
+          JSON.parse(REDIS.get('donators'))
+                   rescue StandardError
+                     []
+        end
+
         if args[0] == 'add'
-          if user.nil?
-            event.respond("#{YuukiBot.config['emoji_error']} Not a valid user!")
-            next
-          end
-          donators = begin
-        JSON.parse(REDIS.get('donators'))
-                     rescue StandardError
-                       []
-      end
           if donators.include?(id)
             event.respond("#{YuukiBot.config['emoji_error']} User is already a donator!")
             next
           end
           REDIS.set('donators', donators.push(id).to_json)
-          event.respond("#{YuukiBot.config['emoji_tickbox']} added `#{event.bot.user(id).nil? ? "Unknown User (ID: #{id})" : event.bot.user(id).distinct.to_s}` to donators!")
+          event.respond("#{YuukiBot.config['emoji_tickbox']} added `#{Helper.userid_to_string(id)}` to donators!")
         elsif args[0] == 'remove'
-          if user.nil?
-            event.respond("#{YuukiBot.config['emoji_error']} Not a valid user!")
-            next
-          end
-          donators = begin
-        JSON.parse(REDIS.get('donators'))
-                     rescue StandardError
-                       []
-      end
           unless donators.include?(id)
             event.respond("#{YuukiBot.config['emoji_error']} User is not a donator!")
             next
           end
-          REDIS.set('donators', donators.delete(id).to_json)
+          REDIS.set('donators', (donators - [id]).to_json)
           event.respond("#{YuukiBot.config['emoji_tickbox']} removed `#{Helper.userid_to_string(user.id)}` from donators!")
-        else
-          if YuukiBot.config['show_donate_urls']
-            event << "💰 Hey, making bots and hosting them isn't free. If you want this bot to stay alive, consider giving some 💵 to the devs: "
-            YuukiBot.config['donate_urls'].each { |url| event << "- #{url}" }
-            event << '__**Donators ❤️**__ (aka the best people ever)'
-            donators = begin
-          JSON.parse(REDIS.get('donators'))
-                       rescue StandardError
-                         []
-        end
-            if !donators.empty?
-              donators.each do |x|
-                event.bot.user(x).nil? ? event << "- Unknown User (ID: `#{x}`)" : event << "- **#{event.bot.user(x).distinct}**"
-              end
-            else
-              event << 'None! You can be the first! :)'
-            end
-          else
-            event << 'Sorry, donation information has been disabled for this bot instance!'
-            event << 'Please contact the bot owner for more information.'
-          end
         end
       },
-      owners_only: true,
-      failcode: proc { |event, _|
-        if YuukiBot.config['show_donate_urls']
-          event << "💰 Hey, making bots and hosting them isn't free. If you want this bot to stay alive, consider giving some 💵 to the devs: "
-          YuukiBot.config['donate_urls'].each { |url| event << "- #{url}" }
-          event << '__**Donators ❤️*__ (aka the best people ever)'
-          donators = begin
-        JSON.parse(REDIS.get('donators'))
-                     rescue StandardError
-                       []
-      end
-          if !donators.empty?
-            donators.each do |x|
-              event.bot.user(x).nil? ? event << "- Unknown User (ID: `#{x}`)" : event << "- **#{event.bot.user(x).distinct}**"
-            end
-          else
-            event << 'None! You can be the first! :)'
-          end
-        else
-          event << 'Sorry, donation information has been disabled for this bot instance!'
-          event << 'Please contact the bot owner for more information.'
-        end
-      }
+      owners_only: true
     )
 
     YuukiBot.crb.add_command(
       :donate,
       code: proc { |event, _args|
-        if YuukiBot.config['show_donate_urls']
-          event << "💰 Hey, making bots and hosting them isn't free. If you want this bot to stay alive, consider giving some 💵 to the devs: "
-          YuukiBot.config['donate_urls'].each { |url| event << "- #{url}" }
-          event << '__**Donators :heart:**__ (aka the best people ever)'
-          donators = begin
-       JSON.parse(REDIS.get('donators'))
-                     rescue StandardError
-                       []
-     end
-          if !donators.empty?
-            donators.each do |x|
-              event.bot.user(x).nil? ? event << "- Unknown User (ID: `#{x}`)" : event << "- **#{event.bot.user(x).distinct}**"
-            end
-          else
-            event << 'None! You can be the first! :)'
-          end
-        else
-          event << 'Sorry, donation information has been disabled for this bot instance!'
-          event << 'Please contact the bot owner for more information.'
-        end
+        common_donate(event)
       },
       triggers: ['donate', 'donateinfo', 'how do i donate', 'how do i donate?', 'how do I donate', 'how do I donate?', 'doante', 'donut']
     )
+
+    def self.common_donate(event)
+      if YuukiBot.config['show_donate_urls']
+        event << "💰 Hey, making bots and hosting them isn't free. If you want this bot to stay alive, consider giving some 💵 to the devs: "
+        YuukiBot.config['donate_urls'].each { |url| event << "- #{url}" }
+        event << '__**Donators :heart:**__ (aka the best people ever)'
+        donators = begin
+  JSON.parse(REDIS.get('donators'))
+                   rescue StandardError
+                     []
+end
+        if donators.empty?
+          event << 'None! You can be the first! :)'
+        else
+          donators.each do |x|
+            event.bot.user(x).nil? ? event << "- Unknown User (ID: `#{x}`)" : event << "- **#{event.bot.user(x).distinct}**"
+          end
+        end
+      else
+        event << 'Sorry, donation information has been disabled for this bot instance!'
+        event << 'Please contact the bot owner for more information.'
+        end
+      end
   end
 end
