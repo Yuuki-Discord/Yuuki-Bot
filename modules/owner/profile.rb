52ccf4758e98345bd29d796c4a0594c735005ba0
Spotlight <spotlight@joscomputing.space>
2020-04-08 14:54:20 -0500

    Fix bot profile config related logic

--- modules/owner/profile.rb
+++ modules/owner/profile.rb
@@ -7,47 +7,57 @@ module YuukiBot
       :set,
       code: proc { |event, args|
         if args.length.zero?
-          event.respond("#{YuukiBot.config['emoji_error']} Enter a valid subcommand!\nValid commands: `avatar`, `game`, `username`, `status`, `nickname`.")
-        else
-          case args[0].downcase
-          when 'nick', 'nickname'
-            begin
-              event.bot.profile.on(event.server).nickname = args.drop(1).join(' ')
-              event.respond("#{YuukiBot.config['emoji_tickbox']} Nickname changed to `#{args.drop(1).join(' ')}`")
-            rescue Discordrb::Errors::NoPermission
-              event.respond("#{YuukiBot.config['emoji_error']} I don't have permission to change my nickname!")
-            end
-          when 'avatar', 'avy'
-            url = args.drop(1).join(' ')
-            event.bot.profile.avatar = File.open(Helper.download_file(url, 'tmp'))
-          when 'game', 'playing'
-            event.bot.game = args.drop(1).join(' ')
-            event.respond("#{YuukiBot.config['emoji_tickbox']} Game set to `#{args.drop(1).join(' ')}`!")
-          when 'username', 'name'
-            username = args.drop(1).join(' ')
-            event.bot.profile.name = begin
-              username
-                                     rescue Error => e
-                                       event.respond("#{YuukiBot.config['emoji_error']} An error has occured!\n```ruby\n#{e}```")
-            end
-            event.respond("#{YuukiBot.config['emoji_tickbox']} Username _should_ be updated!")
-          when 'status', 'indicator', 'state'
-            state = args[1]
-            case state.downcase
-            when 'idle', 'away', 'afk' then event.bot.idle
-            when 'dnd'
-              state = 'Do Not Disturb'
-            when 'online' then event.bot.online
-            when 'invisible', 'offline' then event.bot.invisible
-            else
-              event.respond('Enter a valid argument!')
-              next
-            end
-            event.respond("#{YuukiBot.config['emoji_tickbox']} Status set to **#{state == 'Do Not Disturb' ? state : state.capitalize}**!")
-          else
-            event.respond("#{YuukiBot.config['emoji_error']} Enter a valid subcommand!\nValid commands: `avatar`, `game`, `username`, `status`, `nickname`.")
+          # Let the default case handle as unknown.
+          args[0] = ''
+        end
+
+        error = YuukiBot.config['emoji_error']
+        tickbox = YuukiBot.config['emoji_tickbox']
+
+        case args[0].downcase
+        when 'nick', 'nickname'
+          begin
+            event.bot.profile.on(event.server).nickname = args.drop(1).join(' ')
+            event.respond("#{tickbox} Nickname changed to `#{args.drop(1).join(' ')}`")
+          rescue Discordrb::Errors::NoPermission
+            event.respond("#{error} I don't have permission to change my nickname!")
+          end
+        when 'avatar', 'avy'
+          url = args.drop(1).join(' ')
+          event.bot.profile.avatar = File.open(Helper.download_file(url, 'tmp'))
+        when 'game', 'playing'
+          event.bot.game = args.drop(1).join(' ')
+          event.respond("#{tickbox} Game set to `#{args.drop(1).join(' ')}`!")
+        when 'username', 'name'
+          username = args.drop(1).join(' ')
+          begin
+            event.bot.profile.name = username
+          rescue StandardError => e
+            event.respond("#{error} An error has occured!\n```ruby\n#{e}```")
             next
           end
+
+          event.respond("#{tickbox} Username _should_ be updated!")
+        when 'status', 'indicator', 'state'
+          state = args[1]
+          case state.downcase
+          when 'idle', 'away', 'afk' then event.bot.idle
+          when 'dnd'
+            state = 'Do Not Disturb'
+            event.bot.dnd
+          when 'online' then event.bot.online
+          when 'invisible', 'offline' then event.bot.invisible
+          else
+            event.respond('Enter a valid argument!')
+            next
+          end
+
+          display_state = state == 'Do Not Disturb' ? state : state.capitalize
+          event.respond("#{tickbox} Status set to **#{display_state}**!")
+        else
+          event.respond("#{error} Enter a valid subcommand!\n" \
+            'Valid commands: `avatar`, `game`, `username`, `status`, `nickname`.')
+          next
         end
       },
       triggers: %w[set config],
@@ -57,23 +67,29 @@ module YuukiBot
     YuukiBot.crb.add_command(
       :reset,
       code: proc { |event, args|
+        if args.length.zero?
+          # Let the default case handle as unknown.
+          args[0] = ''
+        end
+
         case args[0].downcase
-        when 'nick' || 'nickname'
+        when 'nick', 'nickname'
           begin
             event.bot.profile.on(event.server).nickname = nil
             event.respond("#{YuukiBot.config['emoji_tickbox']} Reset nickname!")
           rescue Discordrb::Errors::NoPermission
-            event.respond("#{YuukiBot.config['emoji_error']} I don't have permission to change my nickname!")
+            error = YuukiBot.config['emoji_error']
+            event.respond("#{error} I don't have permission to change my nickname!")
           end
-        when 'avatar' || 'avy'
+        when 'avatar', 'avy'
           event.bot.avatar = nil
           event.respond "#{YuukiBot.config['emoji_success']} Reset avatar!"
-        when 'game' || 'playing'
+        when 'game', 'playing'
           event.bot.game = nil
           event.respond("#{YuukiBot.config['emoji_tickbox']} Reset game!")
         else
-          event.respond("#{YuukiBot.config['emoji_error']} Enter a valid subcommand!\nValid commands: `avatar`, `game`, `nickname`.")
-          next
+          event.respond("#{YuukiBot.config['emoji_error']} Enter a valid subcommand!\n" \
+            'Valid commands: `avatar`, `game`, `nickname`.')
         end
       },
       triggers: %w[reset],
