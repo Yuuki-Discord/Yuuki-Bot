e7311932f53f501024974c01db55df174c208ae0
Spotlight <spotlight@joscomputing.space>
2020-04-09 14:11:59 -0500

    Rewrite clearing to merge forceclear and clear into the same message

--- modules/mod/clear.rb
+++ modules/mod/clear.rb
@@ -9,107 +9,94 @@ module YuukiBot
       code: proc { |event, args|
         unless /\A\d+\z/ =~ args[0]
           event.respond("`#{args[0]}` is not a valid number!")
-          break
+          next
         end
-        original_num = args[0].to_i
-        clearnum = original_num + 1
 
-        if clearnum >= 100
+        original_num = args[0].to_i
+        # Include the user's invoking message
+        clear_num = original_num + 1
+
+        message_count = 0
+
+        if clear_num > 1000
+          event.respond('The maximum number of messages I can clear is 1000!')
+          next
+        elsif clear_num >= 100
           warning = YuukiBot.config['emoji_warning']
-          message = "#{warning} You are attempting to clear more than 100 messages.\n" \
-            'To avoid rate limiting, 99 messages will be removed at a time. ' \
-            "It may take a while.\n" \
-            'This message will vanish in 5 seconds and the clearing will begin, please wait..'
-          event.respond(message)
-          sleep(5)
-        elsif clearnum > 1000
-          message = 'The maximum number of messages I can clear is 1000!'
-          event.respond(message)
-          break
+          event.respond("#{warning} Starting deletion. Due to rate limits, this may take a while.")
+          sleep(3)
+
+          # Our warning message increases the amount of messages we need to clean.
+          clear_num += 1
         end
+
+        # Any messages past 2 weeks cannot be deleted.
+        # Create a time two weeks and five minutes behind to account for potential
+        # inconsistencies from Discord. (None have been observed, and we hope to keep it that way.)
+        max_bulk_delete_time = Time.now.utc
+        # Decrement by 5 minutes (5 times 60 seconds)
+        max_bulk_delete_time -= 5 * 60
+        # Decrement by 2 weeks (2 groups of 7 days over 24 hours/60 minutes/60 seconds)
+        max_bulk_delete_time -= 2 * 7 * 24 * 60 * 60
 
         begin
-          while clearnum.positive?
-            ids = []
-            if clearnum >= 99
-              event.channel.history(99).each { |x| ids.push(x.id) }
-              Discordrb::API::Channel.bulk_delete_messages(event.bot.token, event.channel.id, ids)
-              clearnum -= 99
-              sleep(4)
-            else
-              event.channel.history(clearnum).each { |x| ids.push(x.id) }
-              Discordrb::API::Channel.bulk_delete_messages(event.bot.token, event.channel.id, ids)
-              clearnum = 0
+          while clear_num.positive?
+            bulk_ids = []
+            manual_ids = []
+            batch_messages = 0
+
+            to_clear = if clear_num > 100
+                         # We can only potentially bulk-delete messages by 100 at a time.
+                         # Set our current loop's artificial limit.
+                         # See https://discordapp.com/developers/docs/resources/channel#bulk-delete-messages
+                         100
+                       else
+                         clear_num
+                       end
+
+            # Sort messages between being available for bulk and manual per-message deletion.
+            event.channel.history(to_clear).each do |msg|
+              # If the message is younger than two weeks, it should be bulk.
+              if msg.timestamp > max_bulk_delete_time
+                bulk_ids.push(msg.id)
+              else
+                manual_ids.push(msg.id)
+              end
+
+              batch_messages += 1
             end
+
+            unless bulk_ids.empty?
+              channel_id = event.channel.id
+              Discordrb::API::Channel.bulk_delete_messages(event.bot.token, channel_id, bulk_ids)
+            end
+
+            unless manual_ids.empty?
+              manual_ids.each do |id|
+                event.channel.message(id).delete
+              end
+            end
+
+            if to_clear > batch_messages
+              # The user may have specified more messages than were available.
+              clear_num = 0
+            else
+              # All messages from this batch are now cleared.
+              clear_num -= to_clear
+            end
+
+            message_count += batch_messages
           end
-          event.respond("#{YuukiBot.config['emoji_clear']} Cleared #{original_num} messages!\n" \
-            "Responsible Moderator: #{event.user.mention}\n" \
-            '(Messages older than 2 weeks will not have been deleted.' \
-            'You can `forceclear` for those.)')
         rescue Discordrb::Errors::NoPermission
           event.respond("#{YuukiBot.config['emoji_error']} Message delete failed!\n" \
             'Check the permissions?')
-          break
+          next
         end
-        nil
+
+        event.respond("#{YuukiBot.config['emoji_clear']} Cleared #{message_count} messages!\n" \
+            "Responsible Moderator: #{event.user.mention}\n")
       },
       triggers: %w[clear clean],
-      server_only: true,
-      required_permissions: [:manage_messages],
-      owner_override: false,
-      max_args: 1
-    )
-
-    YuukiBot.crb.add_command(
-      :forceclear,
-      code: proc { |event, args|
-        unless /\A\d+\z/ =~ args[0]
-          event.respond("`#{args[0]}` is not a valid number!")
-          break
-        end
-        original_num = args[0].to_i
-        clearnum = original_num + 1
-
-        if clearnum > 20
-          warning = YuukiBot.config['emoji_warning']
-          message = "#{warning} You are attempting a force clear on a large number of messages.\n" \
-            'Force clears are much slower than ordinary clears, ' \
-            "but can delete messages older than 2 weeks.\n" \
-            'This message will vanish in 5 seconds and the clearing will begin, please wait..'
-          event.respond(message)
-          sleep(5)
-        elsif clearnum > 1000
-          message = 'I can only clear a maximum of 1000 messages!'
-          event.respond(message)
-          break
-        end
-
-        begin
-          while clearnum.positive?
-            if clearnum >= 99
-              event.channel.history(99).each(&:delete)
-              clearnum -= 99
-              sleep(4)
-            else
-              event.channel.history(clearnum).each(&:delete)
-              clearnum = 0
-            end
-          end
-          # Emoji below is a trash can icon thingy.
-          event.respond("#{YuukiBot.config['emoji_clear']} Cleared #{original_num} messages!")
-
-          # On second thought, that's annoying.
-
-          # ~ sleep(3)
-          # ~ message.delete
-        rescue Discordrb::Errors::NoPermission
-          event.respond("#{YuukiBot.config['emoji_error']} Message delete failed!\n" \
-            'Check the permissions?')
-          break
-        end
-        nil
-      },
-      triggers: %w[forceclear forceclean],
       server_only: true,
       required_permissions: [:manage_messages],
       owner_override: false,
