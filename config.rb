54e7037646a71999e01aec2047ca592d0c124cdc
Erisa A <seriel@erisa.moe>
2020-06-12 15:41:08 +0100

    Move the initial setup to a separate script

---
+++ config.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'yaml'
+require 'io/console'
+
+puts 'If you want to create a config file manually, please stop this program with Ctrl+C ' \
+    'and follow the instructions in config/README.md.'
+puts 'Press any key to continue!'
+STDIN.getch
+
+# Initialise a fresh set of configuration options.
+newconfig = YAML.load_file('config/config.sample.yml')
+puts '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+puts 'Okay! First of all you\'ll need to create a bot account!'
+puts 'To do that, go here -> https://discordapp.com/developers/applications/me <-'
+puts 'And login to your Discord account! Then follow the instructions to make a new app'
+puts 'After you have your app, select the "Bot" tab and click the blue button ' \
+    'labelled "Add Bot"'
+puts 'Then click "click to reveal" next to \'Token:\' and copy the long string inside!'
+print 'Please paste that string here: '
+token = gets.chomp
+
+puts '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+puts 'Awesome! Now you\'ll need to head back to the "General information" tab' \
+    'and fetch the Client ID, which should be a long number.'
+print 'And paste that here: '
+appid = gets.chomp
+
+puts '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+puts 'Wew, that\'s the hard part out of the way. ' \
+    'Now you get to decide some needed options for your bot!'
+puts 'Your bot will need at least one prefix to be used. For example: ' \
+    'if you use the prefix s!, your help command will be \'s!help\'.'
+puts 'If your prefix is a word, please add a space after it! ' \
+    '(For example enter \'tomoe\' for \'tomoe ping\' instead of \'tomoeping\''
+print 'Enter your first prefix here: '
+prefix = gets.chomp
+
+puts "Nice prefix! Your bots help command will be: '#{prefix}help'"
+
+puts '=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+puts 'That\'s enough information to get the bot launching, but there\'s one more thing we need.'
+puts 'If you haven\'t already, go to User Settings -> Appearance, and enable Developer Mode.'
+puts 'Then, right click your own name and select Copy ID.'
+print 'Paste that ID here: '
+id = gets.chomp
+puts 'Nice! This will make you the owner of the bot and allow you to control things ' \
+    'and add more owners through commands.'
+
+puts "\nPlease note that there's more to verything configured above!"
+puts 'Check config/config.sample.yml and config.yml to fine-tune the options.'
+
+puts "\n We're now collating the information and generating a configuration file..."
+newconfig['token'] = token
+newconfig['client_id'] = appid
+newconfig['prefixes'][0] = prefix
+newconfig['master_owner'] = id
+
+File.write('config/config.yml', newconfig.to_yaml)
+
+puts 'All done! Feel free to launch the bot now!'
--- modules/setup.rb
+++ modules/setup.rb
@@ -9,67 +9,8 @@ module YuukiBot
   # Load Config from YAML
 
   unless File.exist?('config/config.yml')
-    puts 'You don\'t have a valid config file!'
-    puts 'If you want to create a config file manually, please stop this program with Ctrl+C ' \
-      'and follow the instructions in config/README.md.'
-    puts 'Press any key to continue!'
-    STDIN.getch
-
-    # Initialise a fresh set of configuration options.
-    newconfig = YAML.load_file('config/config.sample.yml')
-    puts '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
-    puts 'Okay! First of all you\'ll need to create a bot account!'
-    puts 'To do that, go here -> https://discordapp.com/developers/applications/me <-'
-    puts 'And login to your Discord account! Then follow the instructions to make a new app'
-    puts 'After you have your app, select the "Bot" tab and click the blue button ' \
-      'labelled "Add Bot"'
-    puts 'Then click "click to reveal" next to \'Token:\' and copy the long string inside!'
-    print 'Please paste that string here: '
-    token = gets.chomp
-
-    puts '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
-    puts 'Awesome! Now you\'ll need to head back to the "General information" tab' \
-      'and fetch the Client ID, which should be a long number.'
-    print 'And paste that here: '
-    appid = gets.chomp
-
-    puts '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
-    puts 'Wew, that\'s the hard part out of the way. ' \
-      'Now you get to decide some needed options for your bot!'
-    puts 'Your bot will need at least one prefix to be used. For example: ' \
-      'if you use the prefix s!, your help command will be \'s!help\'.'
-    puts 'If your prefix is a word, please add a space after it! ' \
-      '(For example enter \'tomoe\' for \'tomoe ping\' instead of \'tomoeping\''
-    print 'Enter your first prefix here: '
-    prefix = gets.chomp
-
-    puts "Nice prefix! Your bots help command will be: '#{prefix}help'"
-
-    puts '=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
-    puts 'That\'s enough information to get the bot launching, but there\'s one more thing we need.'
-    puts 'If you haven\'t already, go to User Settings -> Appearance, and enable Developer Mode.'
-    puts 'Then, right click your own name and select Copy ID.'
-    print 'Paste that ID here: '
-    id = gets.chomp
-    puts 'Nice! This will make you the owner of the bot and allow you to control things ' \
-      'and add more owners through commands.'
-
-    puts "\nPlease note that there's more to verything configured above!"
-    puts 'Check config/config.sample.yml and config.yml to fine-tune the options.'
-
-    puts "\n We're now collating the information and generating a configuration file..."
-    newconfig['token'] = token
-    newconfig['client_id'] = appid
-    newconfig['prefixes'][0] = prefix
-    newconfig['master_owner'] = id
-
-    File.write('config/config.yml', newconfig.to_yaml)
-
-    puts 'All done! The bot will launch in 1 second..'
-    # Delay so the user knows what's happening.
-    sleep(1)
-
-    # Let's go!
+    puts 'No valid config file found. Please copy config/config.sample.yml
+    to config/config.yml and edit it, or run config.rb.'
   end
 
   @config = YAML.load_file('config/config.yml')
