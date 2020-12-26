751cf8cf9be5fecee6e645133a69a05840396caa
Spotlight <spotlight@joscomputing.space>
2020-12-26 16:14:28 -0600

    Use native attachment uploading
    Previously, we manually made API calls to Discord in order to use both an embed and a file. This may be unreliable in the future.
    
    However, we cannot use in-memory data directly. The FauxFile object allows a File-like type based on StringIO that is compatible with rest-client.

--- modules/utility/qr.rb
+++ modules/utility/qr.rb
@@ -4,6 +4,34 @@
 
 module YuukiBot
   module Utility
+    # Per https://git.io/JLMwW (rest-client@v2.1.0 lib/restclient/payload.rb#L48-L57),
+    # a file is any class responding to #path and #read. It additionally use basename.
+    # We use StringIO as a parent and implement path and others for File-like functionality.
+    class FauxFile < StringIO
+      @filename = ''
+      @filetype = ''
+
+      def initialize(contents, filename = 'unknown', filetype = 'application/octet-stream')
+        super contents
+
+        @filename = filename
+        @filetype = filetype
+      end
+
+      def path
+        @filename
+      end
+
+      # Path is assumed to only be the specified filename.
+      def original_filename
+        path
+      end
+
+      def content_type
+        @filetype
+      end
+    end
+
     YuukiBot.crb.add_command(
       :qr,
       code: proc { |event, args|
@@ -16,11 +44,14 @@ module YuukiBot
           next
         end
 
-        # Force the size to be 512x512 px.
-        qr_code = RQRCode::QRCode.new(content)
-        png = qr_code.as_png(size: 512)
         filename = 'qr.png'
 
+        # Force the size to be 512x512 px.
+        qr_code = RQRCode::QRCode.new(content)
+        png_data = qr_code.as_png(size: 512)
+        qr_file = FauxFile.new png_data.to_blob, filename, 'image/png'
+
+        # Create an embed to wrap the QR code.
         embed = Discordrb::Webhooks::Embed.new
         embed.colour = 0x74f167
         embed.author = Discordrb::Webhooks::EmbedAuthor.new(
@@ -38,52 +69,11 @@ module YuukiBot
           url: "attachment://#{filename}"
         )
 
-        upload_file_with_embed('', embed, png, filename, event.channel.id)
+        attachments = [qr_file]
+        event.channel.send_message('', false, embed, attachments)
       },
       min_args: 1,
       catch_errors: true
     )
-
-    # https://discordapp.com/developers/docs/resources/channel#create-message-using-attachments-within-embeds
-    # @param [String (frozen)] contents The contents of the message being sent
-    # @param [Object] embed The embed attached to this message
-    # @param [Object] file The contents of the file itself.
-    # @param [String (frozen)] The name of the file to present to Discord.
-    # @param [String (frozen)] The ID of this channel to upload to.
-    def self.upload_file_with_embed(contents = '', embed, file, filename, channel_id)
-      # Create an IO stream for our payload information.
-      payload = {
-        content: contents,
-        tts: false,
-        embed: embed.to_hash,
-        nonce: nil
-      }.to_json
-
-      # The default for to_blob is ASCII-8BIT, where we need UTF-8.
-      file_datastream = file.to_datastream
-      file_writer = StringIO.new
-      file_writer.set_encoding('UTF-8')
-      file_datastream.write(file_writer)
-
-      # Route is POST /channels/{channel.id}/messages
-      route = "#{Discordrb::API.api_base}/channels/#{channel_id}/messages"
-      uri = URI.parse(route)
-
-      form_req = Net::HTTP::Post.new(uri)
-      form_req.set_form([
-                          # It's mandatory to set a filename, otherwise the file
-                          # fails to show up without warning.
-                          ['file', file_writer.string, { filename: filename }],
-                          ['payload_json', payload]
-                        ], 'multipart/form-data')
-      # Format: Authorization => Bot <token>
-      form_req['Authorization'] = YuukiBot.crb.bot.token
-
-      n = Net::HTTP.new(uri.host, uri.port)
-      n.use_ssl = true
-      n.start do |http|
-        http.request(form_req)
-      end
-    end
   end
 end
